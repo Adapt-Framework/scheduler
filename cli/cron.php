2e8724072cddc483433843555d1635c0e6de770e
sion purnell <sp@deliowealth.com>
2016-11-09 10:18:17 +0000

    added timeout

--- cli/cron.php
+++ cli/cron.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 define('ADAPT_PATH', $argv[1]);
-
+sleep(300);
 while (true) {
     $path = ADAPT_PATH . "scheduler/scheduler-1.0.0/cli/process.php";
     $command = 'bash -c "exec nohup setsid ' . $path . ' ' . ADAPT_PATH  . ' > /dev/null 2>&1 &"';
